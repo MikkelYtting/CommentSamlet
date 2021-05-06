a6de6a95dc9f313f73f4b9e1b05c3032cf819cd8
MikkelYtting <Mytting1996@gmail.com>
2021-05-06 14:00:01 +0200

    book mand

---
+++ CommentSectionWeb/Model/CommentContext.cs
@@ -0,0 +1,21 @@
+﻿using CommentSection;
+using Microsoft.EntityFrameworkCore;
+using CommentSectionWeb;
+
+
+
+namespace CommentSectionWeb.Model
+{
+    public class CommentContext
+    {
+        public CommentContext(DbContextOptions<CommentContext> options) 
+            : base(options)
+
+        { }
+
+
+        public DbSet<Comment> Comments { get; set; }
+
+        // another DbSet<Owner> Owners { get; set; }
+    }
+}
